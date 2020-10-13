84a81f23e75a7e3efc95481e5caa6ba440b52ace
dinhdeveloper <dinhtrancntt@gmail.com>
2020-10-13 10:10:40 +0700

    done main home

--- app/src/main/java/com/dinh/helping/activity/HomeActivity.java
+++ app/src/main/java/com/dinh/helping/activity/HomeActivity.java
@@ -18,10 +18,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.canhdinh.lib.recyclerview.ProgressStyle;
+import com.canhdinh.lib.recyclerview.XRecyclerView;
 import com.canhdinh.lib.roundview.RoundTextView;
 import com.canhdinh.lib.viewpager.ViewPagerIndicator;
 import com.dinh.helping.R;
 import com.dinh.helping.adapter.product.ListProductAdapter;
+import com.dinh.helping.adapter.product.ListProductDiscountAdapter;
 import com.dinh.helping.helper.Consts;
 import com.dinh.helping.model.CategoryModel;
 import com.dinh.helping.model.ProductModel;
@@ -36,16 +39,20 @@ public class HomeActivity extends AppCompatActivity {
     private ProductViewModel productViewModel;
 
     private ListProductAdapter productAdapter;
+    private ListProductDiscountAdapter discountAdapter;
 
     private ImageView viewEmptyImageSlider;
     private RelativeLayout layoutSliderImage;
     private ViewPager pager_slider_image;
     private ViewPagerIndicator indicator_pager_slider_image;
-    private TextView tvLoadAllCategory, tvLoadAllProduct;
+    private TextView tvLoadAllCategory, tvLoadAllProduct,tvLoadDiscount;
     private ImageView cvItem1, cvItem2, cvItem3, cvItem4, cvItem5, cvItem6;
-    private RecyclerView rcListProduct;
+    private RecyclerView rcListProductDiscount;
+    private XRecyclerView rcListProduct;
     private RoundTextView tvPriceSale6,tvPriceSale5,tvPriceSale4,tvPriceSale3,tvPriceSale2,tvPriceSale1;
 
+
+    private int times = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,12 +69,12 @@ public class HomeActivity extends AppCompatActivity {
             Glide.with(HomeActivity.this).load(R.drawable.tivi1).error(R.drawable.no_image_full).into(cvItem5);
             Glide.with(HomeActivity.this).load(R.drawable.aoquan2).error(R.drawable.no_image_full).into(cvItem6);
 
-            tvPriceSale1.setText(Consts.decimalFormat.format(64000)+"đ");
-            tvPriceSale2.setText(Consts.decimalFormat.format(86000)+"đ");
-            tvPriceSale3.setText(Consts.decimalFormat.format(43000)+"đ");
-            tvPriceSale4.setText(Consts.decimalFormat.format(130000)+"đ");
-            tvPriceSale5.setText(Consts.decimalFormat.format(534000)+"đ");
-            tvPriceSale6.setText(Consts.decimalFormat.format(129900)+"đ");
+            tvPriceSale1.setText("Giày");
+            tvPriceSale2.setText("Tủ lạnh");
+            tvPriceSale3.setText("Thời trang");
+            tvPriceSale4.setText("Áo quần");
+            tvPriceSale5.setText("Tivi");
+            tvPriceSale6.setText("Áo quần");
         });
 
         productViewModel = ViewModelProviders.of(this).get(ProductViewModel.class);
@@ -75,11 +82,62 @@ public class HomeActivity extends AppCompatActivity {
         productViewModel.getListProduct().observe(this, new Observer<List<ProductModel>>() {
             @Override
             public void onChanged(List<ProductModel> productModels) {
+
+                discountAdapter = new ListProductDiscountAdapter(HomeActivity.this,productModels);
+                rcListProductDiscount.setHasFixedSize(true);
+                rcListProductDiscount.setLayoutManager(new LinearLayoutManager(HomeActivity.this,LinearLayoutManager.HORIZONTAL,false));
+                rcListProductDiscount.setAdapter(discountAdapter);
+                discountAdapter.notifyDataSetChanged();
+
+                loadMore(productModels);
                 productAdapter = new ListProductAdapter(HomeActivity.this,productModels);
                 rcListProduct.setHasFixedSize(true);
                 rcListProduct.setLayoutManager(new GridLayoutManager(HomeActivity.this,2));
                 rcListProduct.setAdapter(productAdapter);
                 productAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+    final int itemLimit = 5;
+    private void loadMore(List<ProductModel> productModels) {
+        rcListProduct.setRefreshProgressStyle(ProgressStyle.BallSpinFadeLoader);
+        rcListProduct.setLoadingMoreProgressStyle(ProgressStyle.BallRotate);
+        rcListProduct.setArrowImageView(R.drawable.iconfont_downgrey);
+        rcListProduct.getDefaultRefreshHeaderView().setRefreshTimeVisible(false);
+
+        rcListProduct.setLoadingListener(new XRecyclerView.LoadingListener() {
+            @Override
+            public void onRefresh() {
+
+            }
+            @Override
+            public void onLoadMore() {
+                if(times < 2){
+                    new Handler().postDelayed(new Runnable(){
+                        public void run() {
+                            for(int i = 0; i < itemLimit ;i++){
+                                productViewModel.getListProduct().getValue();
+                            }
+                            if(rcListProduct != null) {
+                                rcListProduct.loadMoreComplete();
+                                productAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    }, 1000);
+                } else {
+                    new Handler().postDelayed(new Runnable() {
+                        public void run() {
+                            for(int i = 0; i < itemLimit ;i++){
+                                productViewModel.getListProduct().getValue();
+                            }
+                            if(rcListProduct != null) {
+                                rcListProduct.setNoMore(true);
+                                productAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    }, 1000);
+                }
+                times ++;
             }
         });
     }
@@ -89,15 +147,20 @@ public class HomeActivity extends AppCompatActivity {
         layoutSliderImage = findViewById(R.id.layoutSliderImage);
         pager_slider_image = findViewById(R.id.pager_slider_image);
         indicator_pager_slider_image = findViewById(R.id.indicator_pager_slider_image);
+
         tvLoadAllCategory = findViewById(R.id.tvLoadAllCategory);
         tvLoadAllProduct = findViewById(R.id.tvLoadAllProduct);
+        tvLoadDiscount = findViewById(R.id.tvLoadDiscount);
+
         cvItem1 = findViewById(R.id.cvItem1);
         cvItem2 = findViewById(R.id.cvItem2);
         cvItem3 = findViewById(R.id.cvItem3);
         cvItem4 = findViewById(R.id.cvItem4);
         cvItem5 = findViewById(R.id.cvItem5);
         cvItem6 = findViewById(R.id.cvItem6);
+
         rcListProduct = findViewById(R.id.rcListProduct);
+        rcListProductDiscount = findViewById(R.id.rcListProductDiscount);
 
         tvPriceSale1 = findViewById(R.id.tvPriceSale1);
         tvPriceSale2 = findViewById(R.id.tvPriceSale2);
--- app/src/main/java/com/dinh/helping/adapter/product/ListProductDiscountAdapter.java
+++ app/src/main/java/com/dinh/helping/adapter/product/ListProductDiscountAdapter.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -15,30 +14,28 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.canhdinh.lib.labelview.LabelImageView;
-import com.canhdinh.lib.roundview.RoundTextView;
 import com.dinh.helping.R;
 import com.dinh.helping.helper.Consts;
 import com.dinh.helping.model.ProductModel;
 
 import java.util.List;
 
-public class ListProductAdapter extends RecyclerView.Adapter<ListProductAdapter.ViewHolder> {
+public class ListProductDiscountAdapter extends RecyclerView.Adapter<ListProductDiscountAdapter.ViewHolder> {
 
     private Context context;
     private List<ProductModel> list;
 
-    private ListProductListener listener;
+    private ListProductDiscountListener listener;
 
-    public interface ListProductListener {
+    public interface ListProductDiscountListener {
         void onClickItem(ProductModel model);
     }
 
-    public void setListener(ListProductListener listener) {
+    public void setListener(ListProductDiscountListener listener) {
         this.listener = listener;
     }
 
-    public ListProductAdapter(Context context, List<ProductModel> list) {
+    public ListProductDiscountAdapter(Context context, List<ProductModel> list) {
         this.context = context;
         this.list = list;
     }
@@ -46,7 +43,7 @@ public class ListProductAdapter extends RecyclerView.Adapter<ListProductAdapter.
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.custom_item_product_new, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.custom_item_product_discount, parent, false);
         return new ViewHolder(view);
     }
 
@@ -61,10 +58,10 @@ public class ListProductAdapter extends RecyclerView.Adapter<ListProductAdapter.
             holder.tvPrice.setText(Consts.decimalFormat.format(Integer.valueOf(model.getPriceSale()))+" VNĐ");
         }
         if (!TextUtils.isEmpty(model.getProductName())){
-            holder.tvLocation.setText("Hồ Chí Minh");
+            holder.tvDiscount.setText("80%");
         }
         if (!TextUtils.isEmpty(model.getProductImage())){
-            Glide.with(context).load(R.drawable.giay4).error(R.drawable.no_image_full).into(holder.imvCategory);
+            Glide.with(context).load(R.drawable.giay4).error(R.drawable.no_image_full).into(holder.imvProduct);
         }
 
     }
@@ -76,19 +73,17 @@ public class ListProductAdapter extends RecyclerView.Adapter<ListProductAdapter.
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         CardView layout_item;
-        ImageView imvCategory;
-        TextView tvName, tvPrice, tvLocation;
-        ProgressBar progressBar;
+        ImageView imvProduct;
+        TextView tvDiscount, tvPrice, tvName;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             layout_item = itemView.findViewById(R.id.layout_item);
-            imvCategory = itemView.findViewById(R.id.imvCategory);
+            imvProduct = itemView.findViewById(R.id.imvProduct);
             tvName = itemView.findViewById(R.id.tvName);
             tvPrice = itemView.findViewById(R.id.tvPrice);
-            tvLocation = itemView.findViewById(R.id.tvLocation);
-            progressBar = itemView.findViewById(R.id.progressBar);
+            tvDiscount = itemView.findViewById(R.id.tvDiscount);
 
             layout_item.setOnClickListener(view -> {
                 if (listener!=null)
